d12816c98a8b19e8df576d6dc6020149a1c969d2
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-30 15:18:57 +0200

    Added Milestone 3

--- js/script.js
+++ js/script.js
@@ -20,12 +20,22 @@ createApp ({
                     text: 'Pulire la macchina',
                     done: false 
                 }
-            ]
+            ],
+            newContent: ''
         }
     },
     methods: {
         eliminateTask: function(index){
             this.tasks.splice(index, 1);
+        },
+        addTask: function() {
+            if (this.newContent.length > 0) {
+                let newTask = {text: this.newContent, done: false};
+                this.tasks.push(newTask);
+                this.newContent = ''
+            } else {
+                console.warn('No text to add')
+            }
         }
     }
 }).mount('#app')
